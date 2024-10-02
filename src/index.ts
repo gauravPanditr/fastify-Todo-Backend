8d0c213352332e7e3010dee39421a69e83bc0adc
gauravPanditr <gauravpandit7856@gmail.com>
2024-10-02 17:44:26 +0530

    make script setup with dist folder

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
-import fastify from 'fastify'
+import Fastify, { FastifyRequest, FastifyReply } from 'fastify';
 
-const server = fastify()
+const fastify = Fastify();
 
-server.get('/ping', async (request, reply) => {
-  return 'pong\n'
-})
+fastify.get('/ping/:userId', async (request: FastifyRequest, reply: FastifyReply) => {
+      const {userId}=request.params as { userId: string };
+      reply.send(`User ID is: ${userId}`);
+});
 
-server.listen({ port: 8080 }, (err, address) => {
+
+fastify.listen({ port: 3000 }, (err, address) => {
   if (err) {
-    console.error(err)
-    process.exit(1)
+    console.error(err);
+    process.exit(1);
   }
-  console.log(`Server listening at ${address}`)
-})
+  console.log(`Server listening at ${address}`);
+});
